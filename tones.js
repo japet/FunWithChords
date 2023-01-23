a973518e40479dadd3b3aa1eb26b90bb5f30e58b
James Peters <jamespeters.ee@gmail.com>
2023-01-22 23:18:26 -0600

    complete overhaul using new webPiano component
    did work to encapsulate the piano into it's own custom HTML component and used events from that to trigger the oscillators. Also changed the oscillators to just use gain to turn on and off to avoid clipping.

--- tones.js
+++ tones.js
@@ -38,19 +38,55 @@ function selectNote(note) {
     parentDiv.appendChild(newDiv);
 }
 
-function unselectNote(note) {
-    const noteName = note.getAttribute("note");
-    const oscillator = oscillators[noteName];
-    oscillator.stop();
-    oscillator.disconnect();
-    delete oscillators[noteName];
-    let noteBlock = document.querySelector("[noteName="+noteName+"]");
-    noteBlock.remove();
-}
-
 // listen for the play button to be clicked
 function play() {
     audioCtx.resume();
+}
+
+const piano1 = document.querySelector('web-piano');
+
+piano1.addEventListener('key-strike', (event) => {
+    const note = event.detail;
+    const freq = Tonal.Note.freq(note);
+    console.log("strike: "+event.detail);
+    playOscillator(note);
+    checkChord(document.querySelector('web-piano'));
+    //create note info block/card
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("noteCard");
+    newDiv.setAttribute("noteName", note);
+    newDiv.innerHTML = "Note: "+note+"<br> Freq:"+freq.toFixed(2);
+    //add it in an existing div
+    let parentDiv = document.querySelector("#noteBox");
+    parentDiv.appendChild(newDiv);
+});
+
+function playOscillator(note){
+    if(!oscillators[note]){
+        const freq = Tonal.Note.freq(note);
+        oscillators[note] = audioCtx.createOscillator();
+        oscillators[note].frequency.value = freq;
+        gains[note] = audioCtx.createGain();
+        oscillators[note].connect(gains[note]);
+        gains[note].connect(audioCtx.destination);
+        oscillators[note].start(0)
+    }
+    gains[note].gain.value = 0.1;
+}
+
+piano1.addEventListener('key-release', (event) => {
+    const note = event.detail;
+    console.log("release: "+event.detail);
+    stopOscillator(note);
+    checkChord(document.querySelector('web-piano'));
+    let noteBlock = document.querySelector("[noteName="+note+"]");
+    noteBlock.remove();
+});
+
+function stopOscillator(note){
+    if(oscillators[note]){
+        gains[note].gain.value = 0;
+    }
 }
 
 // listen for the pause button to be clicked
@@ -58,34 +94,8 @@ function pause() {
     audioCtx.suspend();
 }
 
-//add event listeners on all the piano keys to know when they are pressed
-pianoKeys.forEach(pianoKey => {
-    pianoKey.addEventListener("click", keypress);
-});
-
-//on key press
-function keypress(){
-    if(!this.classList.contains("selected")){
-        this.classList.add("selected");
-        selectNote(this);
-    }else{
-        this.classList.remove("selected");
-        unselectNote(this);
-    }
-    checkChord(); //check if the set of currently selected notes are a chord
-}
-
-function checkChord(){
-    let divs = document.querySelectorAll('.selected');
-    let array = [];
-    
-    //extract notes from the key divs
-    divs.forEach((div)=> {
-        const noteName = div.getAttribute("note")
-        array.push(noteName);
-    });
-
+function checkChord(piano){
+    array = piano.activeNotes;
     const chord = Tonal.Chord.detect(array);
     document.getElementById("chordDisplay").innerHTML = chord;
-    //document.getElementById("noteDisplay").innerHTML = array;
 }
